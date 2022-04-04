a8b609a60fe79b458eccd85b30d25a8e56bc726c
DaBacy <102991629+DaBacy@users.noreply.github.com>
2022-04-04 12:50:02 -0600

    shake and logo up

--- main.ts
+++ main.ts
@@ -1,3 +1,26 @@
+input.onGesture(Gesture.LogoUp, function () {
+    basic.showIcon(IconNames.Angry)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Skull)
+    basic.clearScreen()
+    basic.showIcon(IconNames.Ghost)
+})
+input.onGesture(Gesture.Shake, function () {
+    for (let index = 0; index < 2; index++) {
+        basic.showIcon(IconNames.SmallDiamond)
+        basic.showIcon(IconNames.Diamond)
+        basic.showLeds(`
+            . # . # .
+            # . . . #
+            . . . . .
+            # . . . #
+            . # . # .
+            `)
+        basic.showIcon(IconNames.Diamond)
+        basic.showIcon(IconNames.SmallDiamond)
+    }
+    basic.showIcon(IconNames.Ghost)
+})
 basic.showLeds(`
     # . # . #
     # . # . #
@@ -5,7 +28,47 @@ basic.showLeds(`
     # . . . #
     # # # # #
     `)
-basic.showString("HIIIIIIII")
 basic.forever(function () {
-	
+    basic.showLeds(`
+        . # . # .
+        # . # . #
+        # . . . #
+        # . . . #
+        # . . . #
+        `)
+    basic.showLeds(`
+        . # # # .
+        # . . . #
+        # . . . #
+        # . . . #
+        . # # # .
+        `)
+    basic.showLeds(`
+        # . . . #
+        # # . . #
+        # . # . #
+        # . . # #
+        # . . . #
+        `)
+    basic.showLeds(`
+        # . . # .
+        # . # . .
+        # # . . .
+        # . # . .
+        # . # . .
+        `)
+    basic.showLeds(`
+        # # # . .
+        # . . . .
+        # # . . .
+        # . . . .
+        # # # . .
+        `)
+    basic.showLeds(`
+        # # # . .
+        # . . . .
+        # # . . .
+        # . . . .
+        # # # . .
+        `)
 })
